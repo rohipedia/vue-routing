c5151d715b47033a0948f8d5ed4f21e1f119d510
vhorarohit <rohit.vhora@gmail.com>
2018-10-14 20:41:11 +0530

    more changes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <ul class="nav nav-pills">
-        <li role="presentation"><router-link to="/">Home</router-link></li>
-        <li role="presentation"><router-link to="/user">User</router-link> </li>
+        <router-link tag="li" to="/" active-class="active" exact><a>Home</a></router-link>
+        <router-link tag="li" to="/user" active-class="active"><a>User</a></router-link>
     </ul>
 </template>
--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -1,3 +1,21 @@
 <template>
-    <h3>Some User Details</h3>
+    <div>
+        <h3>Some User Details</h3>
+        <p>User loaded has id : {{ $route.params.id }}</p>
+        <router-link 
+            
+            :to="{name: 'userEdit', params: { id: $route.params.id }, query: { locale: 'en', q: 100 } }"
+            tag="button"
+            class="btn btn-primary"
+            >Edit User</router-link>
+    </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            id: this.$route.params.id
+        }
+    }
+}
+</script>
